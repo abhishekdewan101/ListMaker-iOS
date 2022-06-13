d9965960004c22c75f14feba773a586fed90aa84
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-06-12 20:06:33 -0700

    Predicate queries for the game detail screen now look for the entries for the list id.

--- ListMaker/Screens/GameListScreen.swift
+++ ListMaker/Screens/GameListScreen.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct GameListScreen: View {
     var list: ListModel
+    @FetchRequest var entries: FetchedResults<ListEntry>
+    @Environment(\.managedObjectContext) var moc
+
+    init(list: ListModel) {
+        _entries = FetchRequest<ListEntry>(sortDescriptors: [],
+                                           predicate: NSPredicate(format: "listId == %@", list.id! as CVarArg))
+        self.list = list
+    }
 
     var body: some View {
         Text("Hello from the game list screen of \(list.id ?? UUID())")
             .navigationTitle(list.title ?? "Untitled")
             .navigationBarTitleDisplayMode(.inline)
+
+        Text("Size of the payload \(entries.count)")
     }
 }
